221b58535376341a388cd76c9a78afa19e68e022
大友香穂里 <kaori@ootomokahosato-no-macbook-air.local>
2019-02-13 16:32:22 +0900

    Initial Commit

---
+++ emoor_data2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  emoor_data2
+//
+//  Created by 大友香穂里 on 2019/02/13.
+//  Copyright © 2019年 大友香穂里. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
